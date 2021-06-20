04142a6fdce143fd1acadf6bda41d55f18da0b18
Adam <azxygaming@gmail.com>
2021-06-19 23:43:29 -0400

    new commit!

---
+++ src/main/java/ex30/App30.java
@@ -0,0 +1,14 @@
+package ex30;
+
+public class App30 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        for (a = 1; a <= 12; ++a) {
+            for (b = 1; b <= 12; ++b) {
+                System.out.printf("%4d", (a*b));
+            }
+            System.out.println();
+        }
+    }
+}
